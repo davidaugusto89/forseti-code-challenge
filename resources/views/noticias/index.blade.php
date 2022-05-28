4e516ede91c6923fb84c5d86b1bcb5bf17f8de97
David Augusto <57589195+davidaugusto89@users.noreply.github.com>
2022-05-28 08:12:45 -0300

    Ajuste página inicial

--- resources/views/noticias/index.blade.php
+++ resources/views/noticias/index.blade.php
@@ -1,18 +1,22 @@
 @extends('layout.main')
 
 @section('content')
-<h2> Últimas 5 notícias importadas</h2>
-<div class="row">
-    @foreach ($noticias as $noticia)
-    <div class="col-12 flex-column align-self-stretch">
-        <div class="card mb-4 box-shadow">
-            <div class="card-body d-flex flex-column">
-                <strong class="card-title">{{ $noticia->manchete  }}</strong>
-                <p class="card-text">{{ date('d/m/Y', strtotime($noticia->data)) }} | {{ Str::limit($noticia->hora, 5, '') }}</p>
-                <p class="card-text">{{ $noticia->link  }}</p>
+@if(!$noticias->isEmpty())
+    <h2> Últimas 5 notícias importadas</h2>
+    <div class="row">
+        @foreach ($noticias as $noticia)
+        <div class="col-12 flex-column align-self-stretch">
+            <div class="card mb-4 box-shadow">
+                <div class="card-body d-flex flex-column">
+                    <strong class="card-title">{{ $noticia->manchete  }}</strong>
+                    <p class="card-text">{{ date('d/m/Y', strtotime($noticia->data)) }} | {{ Str::limit($noticia->hora, 5, '') }}</p>
+                    <p class="card-text">{{ $noticia->link  }}</p>
+                </div>
             </div>
         </div>
+        @endforeach
     </div>
-    @endforeach
-</div>
+@else
+    <h2> Nenhuma notícia importada no sistema!</h2>
+@endif
 @stop
